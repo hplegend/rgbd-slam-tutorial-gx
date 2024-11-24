582e2587d54bf4f0b5f95e16f31c0407ab4596af
hplegend <hepeng0123@126.com>
2024-11-24 10:33:38 +0800

    计算速度

--- part VII/src/slam.cpp
+++ part VII/src/slam.cpp
@@ -37,7 +37,7 @@ using namespace std;
 #include <algorithm> // std::sort
 
 // 把g2o的定义放到前面
-typedef g2o::BlockSolver_6_3 SlamBlockSolver; 
+typedef g2o::BlockSolver_6_3 SlamBlockSolver;
 typedef g2o::LinearSolverEigen< SlamBlockSolver::PoseMatrixType > SlamLinearSolver;
 
 vector<string> listPathFiles(string path);
@@ -51,7 +51,7 @@ void drawCamera(pcl::visualization::PCLVisualizer::Ptr viewer, const  Eigen::Mat
 double normofTransform( cv::Mat rvec, cv::Mat tvec );
 
 // 检测两个帧，结果定义
-enum CHECK_RESULT {NOT_MATCHED=0, TOO_FAR_AWAY, TOO_CLOSE, KEYFRAME}; 
+enum CHECK_RESULT {NOT_MATCHED=0, TOO_FAR_AWAY, TOO_CLOSE, KEYFRAME};
 // 函数声明
 CHECK_RESULT checkKeyframes( FRAME& f1, FRAME& f2, g2o::SparseOptimizer& opti, bool is_loops=false );
 // 检测近距离的回环
@@ -71,7 +71,7 @@ int main( int argc, char** argv )
     vector<string> rgbFilesPath = listPathFiles(rgbDir);
 
     // 所有的关键帧都放在了这里
-    vector< FRAME > keyframes; 
+    vector< FRAME > keyframes;
     // initialize
     cout<<"Initializing ..."<<endl;
     int currIndex = startIndex; // 当前索引为currIndex
@@ -82,7 +82,7 @@ int main( int argc, char** argv )
     CAMERA_INTRINSIC_PARAMETERS camera = getDefaultCamera();
     computeKeyPointsAndDesp( currFrame, detector, descriptor );
     PointCloud::Ptr cloud = image2PointCloud( currFrame.rgb, currFrame.depth, camera );
-    
+
     /******************************* 
     // 新增:有关g2o的初始化
     *******************************/
@@ -93,10 +93,10 @@ int main( int argc, char** argv )
     g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg( blockSolver );
 
     g2o::SparseOptimizer globalOptimizer;  // 最后用的就是这个东东
-    globalOptimizer.setAlgorithm( solver ); 
+    globalOptimizer.setAlgorithm( solver );
     // 不要输出调试信息
     globalOptimizer.setVerbose( false );
-    
+
     // 向globalOptimizer增加第一个顶点
     g2o::VertexSE3* v = new g2o::VertexSE3();
     v->setId( currIndex );
@@ -112,7 +112,7 @@ int main( int argc, char** argv )
     currFrame.tvec = cv::Mat::zeros(1, 3, CV_64F);
     keyframes.push_back( currFrame );
 
-    
+
     double keyframe_threshold = atof( pd.getData("keyframe_threshold").c_str() );
     bool check_loop_closure = pd.getData("check_loop_closure")==string("yes");
     int lostFrameCnt = 0;
@@ -131,59 +131,59 @@ int main( int argc, char** argv )
             //没匹配上，直接跳过
             cout<<RED"Not enough inliers."<<endl;
             lostFrameCnt = lostFrameCnt + 1;
-            if(lostFrameCnt >=1) {// 连续两帧没有跟上
-                // 计算方向向量，计算速度，插入一帧，或者从新开始一帧
-                // 目的是保证地图不断。
-                pcl::visualization::PCLVisualizer::Ptr camViewer(new pcl::visualization::PCLVisualizer("SLAM Camera Visualization"));
-                camViewer->setBackgroundColor(0, 0, 0);
-                camViewer->addCoordinateSystem(1.0); // 参数为坐标轴长度
-
-                camViewer->removeAllPointClouds();
-                camViewer->removeAllShapes();
-
-                Eigen::Matrix4d matrix4D = Eigen::Matrix4d::Identity();
-                pcl::PointXYZ last_O_pos;
-                for (size_t i = 0; i < keyframes.size(); ++i) {
-                    pcl::PointXYZ O_pose;
-                    Eigen::Isometry3d T = cvMat2Eigen( keyframes[i].rvec, keyframes[i].tvec );
-                    //每帧位姿的原点坐标只由变换矩阵中的平移向量得到
-                    O_pose.x = T.translation()[0];
-                    O_pose.y = T.translation()[1];
-                    O_pose.z = T.translation()[2];
-                    if (i > 0)
-                        camViewer->addLine(last_O_pos, O_pose, 255, 255, 255, "trac_" + std::to_string(i));
-
-                    pcl::PointXYZ X;
-                    Eigen::Vector3d Xw =T * (0.1 * Eigen::Vector3d(1, 0, 0));
-                    X.x = Xw[0];
-                    X.y = Xw[1];
-                    X.z = Xw[2];
-                    camViewer->addLine(O_pose, X, 255, 0, 0, "X_" + std::to_string(i));
-
-                    pcl::PointXYZ Y;
-                    Eigen::Vector3d Yw =T * (0.1 * Eigen::Vector3d(0, 1, 0));
-                    Y.x = Yw[0];
-                    Y.y = Yw[1];
-                    Y.z = Yw[2];
-                    camViewer->addLine(O_pose, Y, 0, 255, 0, "Y_" + std::to_string(i));
-
-                    pcl::PointXYZ Z;
-                    Eigen::Vector3d Zw =T * (0.1 * Eigen::Vector3d(0, 0, 1));
-                    Z.x = Zw[0];
-                    Z.y = Zw[1];
-                    Z.z = Zw[2];
-                    camViewer->addLine(O_pose, Z, 0, 0, 255, "Z_" + std::to_string(i));
-
-                    last_O_pos = O_pose;
-
-                //    drawCamera(camViewer, matrix4D, i); // 绘制相机
-                }
-
-                while (!camViewer->wasStopped()) {
-                    camViewer->spinOnce(100);
-                }
-
-            }
+//            if(lostFrameCnt >=1) {// 连续两帧没有跟上
+//                // 计算方向向量，计算速度，插入一帧，或者从新开始一帧
+//                // 目的是保证地图不断。
+//                pcl::visualization::PCLVisualizer::Ptr camViewer(new pcl::visualization::PCLVisualizer("SLAM Camera Visualization"));
+//                camViewer->setBackgroundColor(0, 0, 0);
+//                camViewer->addCoordinateSystem(1.0); // 参数为坐标轴长度
+//
+//                camViewer->removeAllPointClouds();
+//                camViewer->removeAllShapes();
+//
+//                Eigen::Matrix4d matrix4D = Eigen::Matrix4d::Identity();
+//                pcl::PointXYZ last_O_pos;
+//                for (size_t i = 0; i < keyframes.size(); ++i) {
+//                    pcl::PointXYZ O_pose;
+//                    Eigen::Isometry3d T = cvMat2Eigen( keyframes[i].rvec, keyframes[i].tvec );
+//                    //每帧位姿的原点坐标只由变换矩阵中的平移向量得到
+//                    O_pose.x = T.translation()[0];
+//                    O_pose.y = T.translation()[1];
+//                    O_pose.z = T.translation()[2];
+//                    if (i > 0)
+//                        camViewer->addLine(last_O_pos, O_pose, 255, 255, 255, "trac_" + std::to_string(i));
+//
+//                    pcl::PointXYZ X;
+//                    Eigen::Vector3d Xw =T * (0.1 * Eigen::Vector3d(1, 0, 0));
+//                    X.x = Xw[0];
+//                    X.y = Xw[1];
+//                    X.z = Xw[2];
+//                    camViewer->addLine(O_pose, X, 255, 0, 0, "X_" + std::to_string(i));
+//
+//                    pcl::PointXYZ Y;
+//                    Eigen::Vector3d Yw =T * (0.1 * Eigen::Vector3d(0, 1, 0));
+//                    Y.x = Yw[0];
+//                    Y.y = Yw[1];
+//                    Y.z = Yw[2];
+//                    camViewer->addLine(O_pose, Y, 0, 255, 0, "Y_" + std::to_string(i));
+//
+//                    pcl::PointXYZ Z;
+//                    Eigen::Vector3d Zw =T * (0.1 * Eigen::Vector3d(0, 0, 1));
+//                    Z.x = Zw[0];
+//                    Z.y = Zw[1];
+//                    Z.z = Zw[2];
+//                    camViewer->addLine(O_pose, Z, 0, 0, 255, "Z_" + std::to_string(i));
+//
+//                    last_O_pos = O_pose;
+//
+//                //    drawCamera(camViewer, matrix4D, i); // 绘制相机
+//                }
+//
+//                while (!camViewer->wasStopped()) {
+//                    camViewer->spinOnce(100);
+//                }
+//
+//            }
 
 
             break;
@@ -224,7 +224,7 @@ int main( int argc, char** argv )
         default:
             break;
         }
-        
+
     }
 
     // 优化
@@ -248,86 +248,142 @@ int main( int argc, char** argv )
     double gridsize = atof( pd.getData( "voxel_grid" ).c_str() ); //分辨图可以在parameters.txt里调
     voxel.setLeafSize( gridsize, gridsize, gridsize );
 
-    for (size_t i=0; i<keyframes.size(); i++)
-    {
+//    for (size_t i=0; i<keyframes.size(); i++)
+//    {
+//        // 从g2o里取出一帧
+//        g2o::VertexSE3* vertex = dynamic_cast<g2o::VertexSE3*>(globalOptimizer.vertex( keyframes[i].frameID ));
+//        Eigen::Isometry3d pose = vertex->estimate(); //该帧优化后的位姿
+//        PointCloud::Ptr newCloud = image2PointCloud( keyframes[i].rgb, keyframes[i].depth, camera ); //转成点云
+//        // 以下是滤波
+////        voxel.setInputCloud( newCloud );
+////        voxel.filter( *tmp );
+////        pass.setInputCloud( tmp );
+////        pass.filter( *newCloud );
+//        // 把点云变换后加入全局地图中
+//        pcl::transformPointCloud( *newCloud, *tmp, pose.matrix() );
+//        *output += *tmp;
+//        tmp->clear();
+//        newCloud->clear();
+//    }
+
+    // 计算方向向量，计算速度，插入一帧，或者从新开始一帧
+    // 目的是保证地图不断。
+    pcl::visualization::PCLVisualizer::Ptr camViewer(
+            new pcl::visualization::PCLVisualizer("SLAM Camera Visualization"));
+    camViewer->setBackgroundColor(0, 0, 0);
+    camViewer->addCoordinateSystem(0.1); // 参数为坐标轴长度
+
+    camViewer->removeAllPointClouds();
+    camViewer->removeAllShapes();
+
+
+
+    g2o::VertexSE3* vertexR2 = dynamic_cast<g2o::VertexSE3*>(globalOptimizer.vertex( keyframes[keyframes.size()-2].frameID ));
+    Eigen::Isometry3d poseR2 = vertexR2->estimate(); //该帧优化后的位姿
+
+    g2o::VertexSE3* vertexR1 = dynamic_cast<g2o::VertexSE3*>(globalOptimizer.vertex( keyframes[keyframes.size()-1].frameID ));
+    Eigen::Isometry3d poseR1 = vertexR1->estimate(); //该帧优化后的位姿
+
+    Eigen::Vector3d dirR2(poseR2.translation()[0], poseR2.translation()[1], poseR2.translation()[2]);
+    Eigen::Vector3d dirR1(poseR1.translation()[0], poseR1.translation()[1], poseR1.translation()[2]);
+    Eigen::Vector3d add = dirR1 + dirR2;
+    double magnitude = add.norm(); // 计算模
+    Eigen::Vector3d velocity = (add / magnitude) * 0.01;
+    // 计算一个方向向量，和速度
+    // 添加100帧看看效果
+    Eigen::Matrix4d matrix4D = Eigen::Matrix4d::Identity();
+    pcl::PointXYZ last_O_pos;
+    for (size_t i = 0; i < keyframes.size(); ++i) {
         // 从g2o里取出一帧
         g2o::VertexSE3* vertex = dynamic_cast<g2o::VertexSE3*>(globalOptimizer.vertex( keyframes[i].frameID ));
         Eigen::Isometry3d pose = vertex->estimate(); //该帧优化后的位姿
-        PointCloud::Ptr newCloud = image2PointCloud( keyframes[i].rgb, keyframes[i].depth, camera ); //转成点云
-        // 以下是滤波
-//        voxel.setInputCloud( newCloud );
-//        voxel.filter( *tmp );
-//        pass.setInputCloud( tmp );
-//        pass.filter( *newCloud );
-        // 把点云变换后加入全局地图中
-        pcl::transformPointCloud( *newCloud, *tmp, pose.matrix() );
-        *output += *tmp;
-        tmp->clear();
-        newCloud->clear();
+
+        pcl::PointXYZ O_pose;
+        //每帧位姿的原点坐标只由变换矩阵中的平移向量得到
+        O_pose.x = pose.translation()[0];
+        O_pose.y = pose.translation()[1];
+        O_pose.z = pose.translation()[2];
+        cout<<O_pose.x<<","<<O_pose.y<<","<<O_pose.z<<endl;
+        if (i > 0)
+            camViewer->addLine(last_O_pos, O_pose, 255, 255, 255, "trac_" + std::to_string(i));
+
+        pcl::PointXYZ X;
+        Eigen::Vector3d Xw = pose * (0.1 * Eigen::Vector3d(1, 0, 0));
+        X.x = Xw[0];
+        X.y = Xw[1];
+        X.z = Xw[2];
+        camViewer->addLine(O_pose, X, 255, 0, 0, "X_" + std::to_string(i));
+
+        pcl::PointXYZ Y;
+        Eigen::Vector3d Yw = pose * (0.1 * Eigen::Vector3d(0, 1, 0));
+        Y.x = Yw[0];
+        Y.y = Yw[1];
+        Y.z = Yw[2];
+        camViewer->addLine(O_pose, Y, 0, 255, 0, "Y_" + std::to_string(i));
+
+        pcl::PointXYZ Z;
+        Eigen::Vector3d Zw = pose * (0.1 * Eigen::Vector3d(0, 0, 1));
+        Z.x = Zw[0];
+        Z.y = Zw[1];
+        Z.z = Zw[2];
+        camViewer->addLine(O_pose, Z, 0, 0, 255, "Z_" + std::to_string(i));
+
+        last_O_pos = O_pose;
+
+    }
+
+    for (size_t i = 200; i < 400; ++i) {
+
+        g2o::VertexSE3* vertex = dynamic_cast<g2o::VertexSE3*>(globalOptimizer.vertex( keyframes[keyframes.size()-1].frameID ));
+        Eigen::Isometry3d pose = vertex->estimate(); //该帧优化后的位姿
+        pose.translation()[1] = last_O_pos.y + velocity[1];
+
+        pcl::PointXYZ O_pose;
+        //每帧位姿的原点坐标只由变换矩阵中的平移向量得到
+        O_pose.x = pose.translation()[0];
+        O_pose.y = pose.translation()[1];
+        O_pose.z = pose.translation()[2];
+
+        cout<< "aaa:"<<O_pose.x<<","<<O_pose.y<<","<<O_pose.z<<endl;
+
+        camViewer->addLine(last_O_pos, O_pose, 255, 255, 255, "trac_" + std::to_string(i));
+
+        pcl::PointXYZ X;
+        Eigen::Vector3d Xw = pose * (0.1 * Eigen::Vector3d(1, 0, 0));
+        X.x = Xw[0];
+        X.y = Xw[1];
+        X.z = Xw[2];
+        camViewer->addLine(O_pose, X, 255, 0, 0, "X_" + std::to_string(i));
+
+        pcl::PointXYZ Y;
+        Eigen::Vector3d Yw = pose * (0.1 * Eigen::Vector3d(0, 1, 0));
+        Y.x = Yw[0];
+        Y.y = Yw[1];
+        Y.z = Yw[2];
+        camViewer->addLine(O_pose, Y, 0, 255, 0, "Y_" + std::to_string(i));
+
+        pcl::PointXYZ Z;
+        Eigen::Vector3d Zw = pose * (0.1 * Eigen::Vector3d(0, 0, 1));
+        Z.x = Zw[0];
+        Z.y = Zw[1];
+        Z.z = Zw[2];
+        camViewer->addLine(O_pose, Z, 0, 0, 255, "Z_" + std::to_string(i));
+
+        last_O_pos = O_pose;
     }
 
 
 
-//    // 计算方向向量，计算速度，插入一帧，或者从新开始一帧
-//    // 目的是保证地图不断。
-//    pcl::visualization::PCLVisualizer::Ptr camViewer(
-//            new pcl::visualization::PCLVisualizer("SLAM Camera Visualization"));
-//    camViewer->setBackgroundColor(0, 0, 0);
-//    camViewer->addCoordinateSystem(1.0); // 参数为坐标轴长度
-//
-//    camViewer->removeAllPointClouds();
-//    camViewer->removeAllShapes();
-//
-//    Eigen::Matrix4d matrix4D = Eigen::Matrix4d::Identity();
-//    pcl::PointXYZ last_O_pos;
-//    for (size_t i = 0; i < keyframes.size(); ++i) {
-//        // 从g2o里取出一帧
-//        g2o::VertexSE3* vertex = dynamic_cast<g2o::VertexSE3*>(globalOptimizer.vertex( keyframes[i].frameID ));
-//        Eigen::Isometry3d pose = vertex->estimate(); //该帧优化后的位姿
-//
-//        pcl::PointXYZ O_pose;
-//        //每帧位姿的原点坐标只由变换矩阵中的平移向量得到
-//        O_pose.x = pose.translation()[0];
-//        O_pose.y = pose.translation()[1];
-//        O_pose.z = pose.translation()[2];
-//        if (i > 0)
-//            camViewer->addLine(last_O_pos, O_pose, 255, 255, 255, "trac_" + std::to_string(i));
-//
-//        pcl::PointXYZ X;
-//        Eigen::Vector3d Xw = pose * (0.1 * Eigen::Vector3d(1, 0, 0));
-//        X.x = Xw[0];
-//        X.y = Xw[1];
-//        X.z = Xw[2];
-//        camViewer->addLine(O_pose, X, 255, 0, 0, "X_" + std::to_string(i));
-//
-//        pcl::PointXYZ Y;
-//        Eigen::Vector3d Yw = pose * (0.1 * Eigen::Vector3d(0, 1, 0));
-//        Y.x = Yw[0];
-//        Y.y = Yw[1];
-//        Y.z = Yw[2];
-//        camViewer->addLine(O_pose, Y, 0, 255, 0, "Y_" + std::to_string(i));
-//
-//        pcl::PointXYZ Z;
-//        Eigen::Vector3d Zw = pose * (0.1 * Eigen::Vector3d(0, 0, 1));
-//        Z.x = Zw[0];
-//        Z.y = Zw[1];
-//        Z.z = Zw[2];
-//        camViewer->addLine(O_pose, Z, 0, 0, 255, "Z_" + std::to_string(i));
-//
-//        last_O_pos = O_pose;
-//
-//    }
-//
-//    while (!camViewer->wasStopped()) {
-//        camViewer->spinOnce(100);
-//    }
+    while (!camViewer->wasStopped()) {
+        camViewer->spinOnce(100);
+    }
 
 
 //    voxel.setInputCloud( output );
 //    voxel.filter( *tmp );
     //存储
     pcl::io::savePCDFile( "../result.pcd", *tmp );
-    
+
     cout<<"Final map is saved."<<endl;
     return 0;
 }
@@ -337,7 +393,7 @@ FRAME readFrame( int index, ParameterReader& pd )
     FRAME f;
     string rgbDir   =   pd.getData("rgb_dir");
     string depthDir =   pd.getData("depth_dir");
-    
+
     string rgbExt   =   pd.getData("rgb_extension");
     string depthExt =   pd.getData("depth_extension");
 
@@ -429,7 +485,7 @@ void checkNearbyLoops( vector<FRAME>& frames, FRAME& currFrame, g2o::SparseOptim
 {
     static ParameterReader pd;
     static int nearby_loops = atoi( pd.getData("nearby_loops").c_str() );
-    
+
     // 就是把currFrame和 frames里末尾几个测一遍
     if ( frames.size() <= nearby_loops )
     {
@@ -455,7 +511,7 @@ void checkRandomLoops( vector<FRAME>& frames, FRAME& currFrame, g2o::SparseOptim
     static int random_loops = atoi( pd.getData("random_loops").c_str() );
     srand( (unsigned int) time(NULL) );
     // 随机取一些帧进行检测
-    
+
     if ( frames.size() <= random_loops )
     {
         // no enough keyframes, check everyone
